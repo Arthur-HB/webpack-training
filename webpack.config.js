c7eccc5fbd46d6219deb144f29b6b270caf10ae7
arthur augusto hencke berti <arthurhencke@gmail.com>
2025-03-22 14:37:27 -0300

    babel rules

--- webpack.config.js
+++ webpack.config.js
@@ -17,12 +17,24 @@ module.exports = {
                     'css-loader',
                     'sass-loader',
                 ],
+            },
+            {
                 test: /\.css$/i,
                 use: [
                     'style-loader',
                     'css-loader',
                 ],
             },
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env'],
+                    },
+                },
+            }
         ],
     },
     plugins: [
